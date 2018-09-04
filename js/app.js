9b933c189198923ee1fd407e69439503e8f50650
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-09-04 10:10:44 -0500

    Adding message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+//--Splash
 $(document).ready(function () {
     setTimeout(function () {
         $('#splash').fadeOut(400);
     }, 2000);
 });
+
+//--Dropdown navbar menu
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
