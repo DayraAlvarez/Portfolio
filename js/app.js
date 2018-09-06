0b4ce8b27f5bf2d62a00e00bc942592ccd45d6ba
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-09-05 21:02:46 -0500

    Adding firebase API

--- js/app.js
+++ js/app.js
@@ -1,25 +1,11 @@
-//--Splash
+//Splash
 $(document).ready(function () {
     setTimeout(function () {
         $('#splash').fadeOut(400);
     }, 2000);
 });
 
-//--Listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-function submitForm(e){
-    e.preventDefault();
-    console.log(123);
-
-}
-
-
-
-
-
-
-
+//Firebase API
 var config = {
     apiKey: "AIzaSyAwTBAeP408SCZN6FD50NP6SkTW6Mu8BvY",
     authDomain: "dayrafolio.firebaseapp.com",
@@ -29,3 +15,38 @@ var config = {
     messagingSenderId: "352703885079"
   };
   firebase.initializeApp(config);
+
+//Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+//Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+//Submit form
+function submitForm(e){
+    e.preventDefault();
+    
+//Get values
+var name = getInputVal('name');
+var email = getInputVal('email');
+var message = getInputVal('message');
+
+//Save message
+saveMessage(name, email, message);
+}
+
+//Function to get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+//Save message to firebase
+function saveMessage (name, email, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+}
+
