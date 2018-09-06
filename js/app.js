27aa1b1b8b91b1421fd610c9b3c75bd1944ad089
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-09-05 20:38:06 -0500

    adding form functionality

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ $(document).ready(function () {
     }, 2000);
 });
 
+//--Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    console.log(123);
+
+}
+
+
+
+
+
+
+
 var config = {
     apiKey: "AIzaSyAwTBAeP408SCZN6FD50NP6SkTW6Mu8BvY",
     authDomain: "dayrafolio.firebaseapp.com",
