2dd9df078ef7608998e91159fb8c7eb15acda490
Dayra Alvarez <dayra@ivedha.com>
2019-10-07 22:16:34 -0500

    need to ellign

--- js/app.js
+++ js/app.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
     }, 3500);
 });
 
-;
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("topnavDrop");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
