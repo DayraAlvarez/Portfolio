c0884f4107adb0f2b5aa02b31403c1e1656acb47
Dayra Alvarez <dayra@ivedha.com>
2019-10-08 00:47:12 -0500

    Fix

--- js/app.js
+++ js/app.js
@@ -4,13 +4,4 @@ $(document).ready(function () {
         $('#splash').fadeOut(600);
     }, 3500);
 });
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("topnavDrop");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+;
